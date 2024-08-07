41d7b7ce061457541060b5de6f89f690c793733e
Gregarious Gamer <gregariousgamer0.gmail.com>
2024-08-07 13:50:15 -0400

    yes! logic working baby! small 20x20 setup working

--- UI/MainMenu/main.gd
+++ UI/MainMenu/main.gd
@@ -66,15 +66,16 @@ func draw_block() -> void:
 		add_child(new_block)
 
 		block_number += 1
+
 		
 func new_box_spawn_info(block_id: int) -> void:
 	# modulate is the key to changing colors, not color oddly
 	var inst: Object = instance_from_id(block_id)
-	GlobalVars.block_number_dict[block_id] = block_number
-	GlobalVars.block_color_dict[block_id] = inst.modulate
-	GlobalVars.block_position_dict[block_id] = inst.position
-	
+	GlobalVars.block_number.append(block_number)
+	GlobalVars.block_color.append(inst.modulate)
+	GlobalVars.block_position.append(inst.position)
 	GlobalVars.block_id.append(block_id)
+	
 	if inst.modulate == GlobalVars.gray:
 		GlobalVars.block_on_off.append(2)
 	elif inst.modulate == GlobalVars.white:
@@ -89,12 +90,19 @@ func change_color(block_id: int) -> void:
 	
 	var block_id_find: int = GlobalVars.block_id.find(block_id)
 	
+	var block_num: int = GlobalVars.block_number.find(block_id_find)
+	
 	if GlobalVars.block_on_off[block_id_find] == 0:
 		inst.modulate = GlobalVars.red
+		GlobalVars.block_color[block_id_find] = GlobalVars.red
 		GlobalVars.block_on_off[block_id_find] = 1
+		
 	elif GlobalVars.block_on_off[block_id_find] == 1:
 		inst.modulate = GlobalVars.white
 		GlobalVars.block_on_off[block_id_find] = 0
+		GlobalVars.block_color[block_id_find] = GlobalVars.white
+		
+	print(block_id, "\t", block_id_find, "\t", block_num)
 		
 		
 func forward_1() -> void:
@@ -104,16 +112,102 @@ func forward_1() -> void:
 	# Oh my gawd it actually changes colors lawd in heaven
 	# for skipping forward one step in time
 	
-	var block_on_off_copy: Array = []
+	var block_on_off_temp: Array = []
+	var count: int
 	
-	for i: int in range(0, 380):
+	for i: int in range(0, 400):
+
 		if i <= 20:
-			block_on_off_copy.append(GlobalVars.block_on_off[i])
-		else:
+			block_on_off_temp.append(2)
+			
+		elif (count < 379
+		&& !gray_blocks.has(count)):
+			var alive_count: int
 			# block being worked on
 			var f1: int = GlobalVars.block_on_off[i + 1]
 			var b1: int = GlobalVars.block_on_off[i - 1]
-			#print(GlobalVars.block_id[i], "\t", f1, "\t", b1, "\t", GlobalVars.block_on_off[i])
+			
+			var f19: int = GlobalVars.block_on_off[i + 19]
+			var b19: int = GlobalVars.block_on_off[i - 19]
+			
+			var f20: int = GlobalVars.block_on_off[i + 20]
+			var b20: int = GlobalVars.block_on_off[i - 20]
+			
+			var f21: int = GlobalVars.block_on_off[i + 21]
+			var b21: int = GlobalVars.block_on_off[i - 21]
+			
+			#print(GlobalVars.block_number[i], "\t", GlobalVars.block_id[i], "\t", f1, "\t", b1, "\t", GlobalVars.block_on_off[i])
+			
+			if GlobalVars.block_on_off[i] == 1:
+				if f1 == 1:
+					alive_count += 1
+				if b1 == 1:
+					alive_count += 1
+				if f19 == 1:
+					alive_count += 1
+				if b19 == 1:
+					alive_count += 1
+				if f20 == 1:
+					alive_count += 1
+				if b20 == 1:
+					alive_count += 1
+				if f21 == 1:
+					alive_count += 1
+				if b21 == 1:
+					alive_count += 1
 				
+				if alive_count == 2 || alive_count == 3:
+					block_on_off_temp.append(1)
+	
+				else:
+					block_on_off_temp.append(0)
+				alive_count = 0
+			
+			if GlobalVars.block_on_off[i] == 0:
+				if f1 == 1:
+					alive_count += 1
+				if b1 == 1:
+					alive_count += 1
+				if f19 == 1:
+					alive_count += 1
+				if b19 == 1:
+					alive_count += 1
+				if f20 == 1:
+					alive_count += 1
+				if b20 == 1:
+					alive_count += 1
+				if f21 == 1:
+					alive_count += 1
+				if b21 == 1:
+					alive_count += 1
+				
+				if alive_count == 3:
+					block_on_off_temp.append(1)
+	
+				else:
+					block_on_off_temp.append(0)
+				alive_count = 0
+			
+				
+		elif (count <= 379
+		&& gray_blocks.has(count)):
+			block_on_off_temp.append(2)
+		elif count > 379:
+			block_on_off_temp.append(2)
 		
+		count += 1
+	
+	GlobalVars.block_on_off = block_on_off_temp		
+	for i: int in range(0, 400):
+		var inst: Object = instance_from_id(GlobalVars.block_id[i])
+
+		if GlobalVars.block_on_off[i] == 1:
+			inst.modulate = GlobalVars.red
+			GlobalVars.block_color[i] = GlobalVars.red
+			
+		if GlobalVars.block_on_off[i] == 0:
+			inst.modulate = GlobalVars.white
+			GlobalVars.block_color[i] = GlobalVars.white
+			
+
 
--- scripts/globals/global_vars.gd
+++ scripts/globals/global_vars.gd
@@ -5,10 +5,9 @@ var white: Color = Color(1, 1, 1, 1)
 var red: Color = Color(1, 0.169, 0.078, 1)
 var gray: Color = Color(0.761, 0.773, 0.8, 1)
 
-var block_number_dict: Dictionary = {}
-var block_color_dict: Dictionary = {}
-var block_position_dict: Dictionary = {}
-
+var block_number: Array = []
+var block_color: Array = []
+var block_position: Array = []
 var block_on_off: Array = []
 var block_id: Array = []
 
